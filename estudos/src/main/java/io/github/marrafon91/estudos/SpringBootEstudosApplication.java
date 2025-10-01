11ff067e36729cdf9b9954ac927fc3a0670b6b81
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-10-01 14:35:26 -0300

    Criando operacao de instanciacao

--- estudos/src/main/java/io/github/marrafon91/estudos/SpringBootEstudosApplication.java
+++ estudos/src/main/java/io/github/marrafon91/estudos/SpringBootEstudosApplication.java
@@ -1,13 +1,29 @@
 package io.github.marrafon91.estudos;
 
+import io.github.marrafon91.estudos.entities.Categoria;
+import io.github.marrafon91.estudos.repositories.CategoriaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBootEstudosApplication {
+public class SpringBootEstudosApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepositorio categoriaRepositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEstudosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+
+		categoriaRepositorio.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
