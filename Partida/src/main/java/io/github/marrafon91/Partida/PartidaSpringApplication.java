7bdf210dbe3a658265e8f7cc3d240a2d096eb19c
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-10-05 07:28:57 -0300

    Create a project, and connection with H2 mem:db

---
+++ Partida/src/main/java/io/github/marrafon91/Partida/PartidaSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.Partida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartidaSpringApplication.class, args);
+	}
+
+}
