8fadd58b5abbd45ff1a7dca62ab436a81a3dc3a0
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-03-12 09:59:06 -0300

    Arrumando o service do Estadio

--- Projetos-SpringBoot/campeonato/src/main/java/io/github/com/campeonato/services/EstadioService.java
+++ Projetos-SpringBoot/campeonato/src/main/java/io/github/com/campeonato/services/EstadioService.java
@@ -1,6 +1,7 @@
 package io.github.com.campeonato.services;
 
 import io.github.com.campeonato.dtos.EstadioDTO;
+import io.github.com.campeonato.entities.Endereco;
 import io.github.com.campeonato.entities.Estadio;
 import io.github.com.campeonato.exceptions.EstadioNotFoundException;
 import io.github.com.campeonato.repositories.EstadioRepository;
@@ -42,7 +43,19 @@ public class EstadioService {
 
     @Transactional
     public EstadioDTO save(EstadioDTO dto) {
-        Estadio estadio = new Estadio(null, dto.nome(), null);
+        Estadio estadio = new Estadio();
+        estadio.setNome(dto.nome());
+        
+        // Criar endereço a partir do EnderecoDTO
+        Endereco endereco = new Endereco();
+        endereco.setLogradouro(dto.endereco().logradouro());
+        endereco.setNumero(dto.endereco().numero());
+        endereco.setComplemento(dto.endereco().complemento());
+        endereco.setBairro(dto.endereco().bairro());
+        endereco.setCidade(dto.endereco().cidade());
+        endereco.setEstado(dto.endereco().estado());
+        
+        estadio.setEndereco(endereco);
         Estadio savedEstadio = repository.save(estadio);
         return new EstadioDTO(savedEstadio);
     }
@@ -52,6 +65,22 @@ public class EstadioService {
         Estadio estadio = repository.findById(id)
                 .orElseThrow(() -> new EstadioNotFoundException("Estádio com ID " + id + " não encontrado"));
         estadio.setNome(dto.nome());
+        
+        // Atualizar endereço se fornecido
+        if (dto.endereco() != null) {
+            Endereco endereco = estadio.getEndereco();
+            if (endereco == null) {
+                endereco = new Endereco();
+                estadio.setEndereco(endereco);
+            }
+            endereco.setLogradouro(dto.endereco().logradouro());
+            endereco.setNumero(dto.endereco().numero());
+            endereco.setComplemento(dto.endereco().complemento());
+            endereco.setBairro(dto.endereco().bairro());
+            endereco.setCidade(dto.endereco().cidade());
+            endereco.setEstado(dto.endereco().estado());
+        }
+        
         Estadio updatedEstadio = repository.save(estadio);
         return new EstadioDTO(updatedEstadio);
     }
