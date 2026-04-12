8db8130847f3ad7885ee6b4c798c7598b1790301
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-12 19:18:58 -0300

    Swagger para os times

--- Projetos-SpringBoot/campeonato/src/main/java/io/github/com/campeonato/controllers/TimeJogadoresController.java
+++ Projetos-SpringBoot/campeonato/src/main/java/io/github/com/campeonato/controllers/TimeJogadoresController.java
@@ -2,6 +2,9 @@ package io.github.com.campeonato.controllers;
 
 import io.github.com.campeonato.dtos.TimeJogadoresDTO;
 import io.github.com.campeonato.services.TimeJogadoresService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,24 +13,36 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/times")
+@Tag(name = "Times", description = "Controle de Times")
 public class TimeJogadoresController {
 
     @Autowired
     private TimeJogadoresService service;
 
-    @GetMapping
+    @Operation(description = "Times", summary = "Todos os Times", responses = {
+            @ApiResponse(description = "Ok", responseCode = "200")
+    })
+    @GetMapping(produces = "application/json")
     public ResponseEntity<List<TimeJogadoresDTO>> allTimeJogadores() {
         List<TimeJogadoresDTO> list = service.findAllTimes();
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping("/{id}")
+    @Operation(description = "Times", summary = "Busca os Times por ID", responses = {
+            @ApiResponse(description = "Ok", responseCode = "200"),
+            @ApiResponse(description = "Not Found", responseCode = "404")
+    })
+    @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<TimeJogadoresDTO> findById(@PathVariable Long id) {
         TimeJogadoresDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 
-    @GetMapping("/nomeTime")
+    @Operation(description = "Times", summary = "Busca os Times por Nome", responses = {
+            @ApiResponse(description = "Ok", responseCode = "200"),
+            @ApiResponse(description = "Not Found", responseCode = "404")
+    })
+    @GetMapping(value = "/nomeTime", produces = "application/json")
     public ResponseEntity<List<TimeJogadoresDTO>> findByNome(
             @RequestParam(required = false) String nome) {
 
